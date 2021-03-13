407f70a36978c4a862c15de951d0d3fea2aa1a94
Engin Demiroğ <engindemirog@gmail.com>
2021-03-13 22:40:11 +0300

    13 Mart Commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full", component:ProductComponent},
+  {path:"products", component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories : Category[]=[];
+  currentCategory :Category;
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -19,8 +20,18 @@ export class CategoryComponent implements OnInit {
   getCategories() {
     this.categoryService.getCategories().subscribe(response=>{
       this.categories = response.data
-    })
-     
+    })   
+  }
+  setCurrentCategory(category:Category){
+    this.currentCategory = category;
+  }
+
+  getCurrentCategoryClass(category:Category){
+    if(category ==this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 
 }
