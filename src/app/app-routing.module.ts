e3dd8955ff8be369cd026fc96ec0a64d64d63e37
Engin Demiroğ <engindemirog@gmail.com>
2021-03-13 23:22:46 +0300

    13 Mart Commit 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
-  {path:"products", component:ProductComponent}
+  {path:"products", component:ProductComponent},
+  {path:"products/category/:categoryId", component:ProductComponent}
 ];
 
 @NgModule({
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,11 +9,17 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-  apiUrl = 'https://localhost:44314/api/products/getall';
+  apiUrl = 'https://localhost:44314/api/';
 
   constructor(private httpClient: HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
