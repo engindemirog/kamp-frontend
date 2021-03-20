f71f67ca9b55b5c26150c76ddd68a007d776a772
Engin Demiroğ <engindemirog@gmail.com>
2021-03-20 21:55:34 +0300

    20 Mart Commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
   {path:"products", component:ProductComponent},
-  {path:"products/category/:categoryId", component:ProductComponent}
+  {path:"products/category/:categoryId", component:ProductComponent},
+  {path:"products/add", component:ProductAddComponent}
 ];
 
 @NgModule({
