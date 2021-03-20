609b1bea75ac39732d7661d4969c85336e1dcac4
Engin Demiroğ <engindemirog@gmail.com>
2021-03-20 22:43:52 +0300

    20 Mart Commit 3

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 
 @Component({
@@ -10,7 +12,8 @@ import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
 export class ProductAddComponent implements OnInit {
 
   productAddForm : FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, 
+    private productService:ProductService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -23,6 +26,23 @@ export class ProductAddComponent implements OnInit {
        unitsInStock:["", Validators.required],
        categoryId:["",Validators.required]
      })
+  }
+
+  add(){
+    if(this.productAddForm.valid){
+      let productModel = Object.assign({},this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response)
+        this.toastrService.success(response.message,"Başarılı")
+      },responseError=>{
+        console.log(responseError.error)
+        this.toastrService.error(responseError.error)
+      })
+      
+    }else{
+      this.toastrService.error("Formunuz eksik","Dikkat")
+    }
+    
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/ListResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,9 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
   }
 }
