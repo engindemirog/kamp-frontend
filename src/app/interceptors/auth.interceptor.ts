b90e6aedb7d7a5f76fd614101c8e349a3a1f9db6
Engin Demiroğ <engindemirog@gmail.com>
2021-03-24 23:03:47 +0300

    24 Mart Commit 4

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let token = localStorage.getItem("token");
+    let newRequest : HttpRequest<any>;
+    newRequest = request.clone({
+      headers: request.headers.set("Authorization","Bearer " + token)
+    })
+    return next.handle(newRequest);
+  }
+}
+
+//Error
+
+//23.15 Dersteyiz
